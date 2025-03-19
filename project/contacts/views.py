5307ec17fd610042b4018f2b0d11cdd5db96353a
cervenkovapavlina <cervenkovapavlina@seznam.cz>
2025-03-19 21:33:39 +0100

    Add model Contact. Adjust detail and list methods for contacts.

--- project/contacts/views.py
+++ project/contacts/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from contacts.models import Contact
 
 # Create your views here.
 
 
-# def list(request):
-#     return render(request, "contacts_list.html")
-
 def list(request):
-    template = loader.get_template("contacts_list.html")
-    return HttpResponse(template.render())
+    contacts = Contact.objects.all()
+    context = {"contacts_list": contacts}
+    return render(request, "contacts_list.html", context)
+
 
 def detail(request, id):
-    context = {"id": id}
-    return render(request, "contacts_detail.html", context)
+    try:
+        contact = Contact.objects.get(id=id)
+        context = {"contact": contact}
+        return render(request, "contact_detail.html", context)
+    except:
+        return render(request, "contacts_404.html", status=404)
