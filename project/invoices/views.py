849af3fd6524d3b006fd8908b227d3268471a394
cervenkovapavlina <cervenkovapavlina@seznam.cz>
2025-04-05 14:17:27 +0200

    Refactor number_row_list serialization

--- project/invoices/views.py
+++ project/invoices/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core.serializers import serialize
 from django.template import loader
-from django.core import serializers
 import json
 from invoices.models import Invoice
 from invoices.models import NumberRow
 from django.views.decorators.csrf import csrf_exempt
 
-
 # Create your views here.
+
 
 def index(request):
     return HttpResponse(json.dumps({}))
 
+
 def number_row_list(request):
-    number_rows = NumberRow.objects.all().values()
-    data = []
-    for item in number_rows:
-        data.append({"id": item["id"], "prefix": item["prefix"], "order": item["order"]})
-    return HttpResponse(json.dumps(data))
+    return JsonResponse(serialize('python', NumberRow.objects.all()), safe=False)
 
 
 def detail(request, id):
@@ -29,20 +26,20 @@ def detail(request, id):
     except:
         return render(request, "404.html", status=404)
 
+
 def report(request):
     numbers = [1, 2, 3, 4]
     # numbers = {"number1": 1, "number2": 2, "number3": 3, "number4": 4}
     return HttpResponse(json.dumps(numbers))
 
+
 @csrf_exempt
 def number_row_create(request):
     if request.method == "POST":
         json_data = json.loads(request.body)
-        number_row = NumberRow()
-        number_row.prefix = json_data["prefix"]
+        number_row = NumberRow(prefix=json_data["prefix"])
         number_row.save()
         return HttpResponse(json.dumps(number_row.id))
     return render(request, "405.html", status=405)
-
 
 
