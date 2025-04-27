bba7499daa0ad4afa75259acec8968aaa4b6be8e
cervenkovapavlina <cervenkovapavlina@seznam.cz>
2025-04-27 20:14:54 +0200

    Adjust NumberRowPrefix and NumberRowValue models according to new design.

--- project/invoices/models.py
+++ project/invoices/models.py
@@ -24,33 +24,28 @@ class Entity(models.Model):
 
 
 class NumberRowPrefix(Entity):
-    prefix = models.CharField(max_length=10, default="", unique=False, null=True)
-    year = models.CharField(max_length=4, default=str(datetime.now().year), unique=False, null=False)
+    prefix = models.CharField(max_length=10, default="", unique=False)
     name = models.CharField(max_length=50, null=False, unique=True)
     received = models.BooleanField(default=True)
 
-
-
-    def get_final_prefix(self):
-        return f"{self.prefix}{self.year}"
-
     def save(self, *args, **kwargs):
-        final_prefix_already_exists = NumberRowPrefix.objects.filter(prefix=self.prefix, year=self.year).count() > 0
-        if final_prefix_already_exists:
-            raise ValidationError("Final prefix already exists.")
+        prefix_already_exists = NumberRowPrefix.objects.filter(prefix=self.prefix, received=self.received).count() > 0
+        if prefix_already_exists:
+            raise ValidationError("Prefix already exists for the selected invoice type.")
         super(NumberRowPrefix, self).save(*args, **kwargs)
 
 
 class NumberRowValue(Entity):
     value = models.IntegerField(unique=False, null=False)
     prefix = models.ForeignKey(NumberRowPrefix, on_delete=models.CASCADE, null=False)
+    year = models.CharField(max_length=4, default=str(datetime.now().year), unique=False, null=False)
 
     def get_final_value(self):
-        return f"{self.prefix.get_final_prefix()}{self.value:04}"
+        return f"{self.prefix.prefix}{self.year}{self.value:04}"
 
     def save(self, *args, **kwargs):
         if self._state.adding:
-            self.value = NumberRowValue.objects.filter(prefix=self.prefix).count() + 1
+            self.value = NumberRowValue.objects.filter(prefix=self.prefix, year=self.year).count() + 1
         super(NumberRowValue, self).save(*args, **kwargs)
 
 
