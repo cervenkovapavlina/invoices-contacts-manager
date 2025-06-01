cfa642d4daef260c4d2731d598af19f7a9b8cee5
cervenkovapavlina <cervenkovapavlina@seznam.cz>
2025-06-01 21:13:03 +0200

    13: Load data from API enpoints

--- frontend/invoices/src/components/issued-invoices/IssuedInvoiceDetail.js
+++ frontend/invoices/src/components/issued-invoices/IssuedInvoiceDetail.js
@@ -1,25 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import DjangoClient from 'utils/DjangoClient';
 
 function IssuedInvoiceDetail() {
     const { id } = useParams()
     const [invoice, setInvoice] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
-    useEffect(() => {
-        setInvoice(id);
-        setLoading(false);
-    }, [id]);
+    function loadData(){
+        let client = new DjangoClient();
+        client.get('number_rows/' + id, (data)=>{
+            setInvoice(data);
+            setLoading(false)
+        }, (error)=>{
+            setLoading(false)
+            setErrorMessage(error)
+        })
+    }
+
+    useEffect(()=>{
+        loadData()
+    }, [])
 
     if (loading) {
         return <div>Loading invoices...</div>;
     }
 
+    if (errorMessage) {
+        return <div>Error: {errorMessage}</div>;
+    }
+
     return (
         <div className="invoice-detail">
             <h2>
-                Issued invoice: {invoice}
+                Issued invoice: {invoice.pk}
             </h2>
+            <p>{invoice.fields.name}</p>
         </div>
     );
 }
--- frontend/invoices/src/components/issued-invoices/IssuedInvoiceList.js
+++ frontend/invoices/src/components/issued-invoices/IssuedInvoiceList.js
@@ -3,27 +3,26 @@ import RestClient from "utils/RestClient";
 import IssuedInvoiceModel from "components/issued-invoices/IssuedInvoiceModel";
 import IssuedInvoiceDetail from "components/issued-invoices/IssuedInvoiceDetail";
 import { Link } from "react-router-dom";
+import DjangoClient from 'utils/DjangoClient';
 
 
 function IssuedInvoiceList(){
   const [invoices, setInvoices] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   const [selectedInvoice, setSelectedInvoice] = useState(null)
 
-  function generateTestData(){
-    let invoices = [];
-    for(let i=1; i<10; i++){
-        let item = new IssuedInvoiceModel(i, "2025000" + i, "11111111", new Date(), "Customer - " + i);
-        invoices.push(item);
-    }
-    return invoices;
+
+  function loadData(){
+    let client = new DjangoClient();
+    client.get('number_rows', (data)=>{
+        setInvoices(data);
+        setLoading(false)
+    }, (error)=>{setErrorMessage(error)})
   }
 
   useEffect(()=>{
-        let data = generateTestData();
-        setInvoices(data);
-        setLoading(false);
+        loadData()
   }, [])
 
   if (loading) return <div>Loading invoices...</div>;
@@ -36,20 +35,14 @@ function IssuedInvoiceList(){
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>invoiceNumber</th>
-                    <th>variableSymbol</th>
-                    <th>issuedDate</th>
-                    <th>customer</th>
+                    <th>name</th>
                 </tr>
             </thead>
             <tbody>
                 {invoices.map((item) => (
-                    <tr key={item.id}>
-                        <td><Link to={`/issued-invoice-detail/${item.id}`}>{item.id}</Link></td>
-                        <td>{item.invoiceNumber}</td>
-                        <td>{item.variableSymbol}</td>
-                        <td>{item.issuedDate.toLocaleDateString()}</td>
-                        <td>{item.customer}</td>
+                    <tr key={item.pk}>
+                        <td><Link to={`/issued-invoice-detail/${item.pk}`}>{item.pk}</Link></td>
+                        <td>{item.fields.name}</td>
                     </tr>
                 ))}
             </tbody>
