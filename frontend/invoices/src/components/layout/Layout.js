0daf77f7bcfe19a56974921405a1383d774a92e0
cervenkovapavlina <cervenkovapavlina@seznam.cz>
2025-06-08 20:43:49 +0200

    13: Layout providdes Outlet

--- frontend/invoices/src/components/layout/Layout.js
+++ frontend/invoices/src/components/layout/Layout.js
@@ -3,23 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav class="d-none">
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/data">Data</Link>
-          </li>
-          <li>
-            <Link to="/data2">Data2</Link>
-          </li>
-          <li>
-            <Link to="/issued-invoices">Issued invoices</Link>
-          </li>
-        </ul>
-      </nav>
-
       <Outlet />
     </>
   )
